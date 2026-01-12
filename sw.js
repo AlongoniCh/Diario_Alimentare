76fd49520062ac6d1cdd0249aa4cb4938b866d92
AlongoniCh <alongoni@redbit.ch>
2026-01-12 10:27:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,4 @@
-const CACHE_NAME = 'diario-food-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then(keys => 
-        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ));
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-            return caches.open(CACHE_NAME).then(c => {
-                c.put(e.request, res.clone());
-                return res;
-            });
-        })).catch(() => caches.match('/index.html'))
-    );
-});
+const CACHE='diario-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html','manifest.json'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('index.html')));});
